6016622bba12e08071c8dbae32d381f7af3a43f5
Vinícius Lima <vinislima@gmail.com>
2022-01-30 22:27:04 -0300

    projeto estudo vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer id="Footer">
+        <p>Make Your Burger &copy; 2021</p>
+    </footer>    
+</template>
+
+<script>
+export default {
+    name:"Footer"
+}
+
+</script>
+
+<style scoped>
+    #footer {
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #FCBA03;
+        display: flex;
+        align-items: center;
+        width: 100%;
+        position: absolute;
+        bottom: 0;
+    }
+</style>
